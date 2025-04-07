53e24f9999b616c04a0cb28a1c18ed11d2173028
Harrizon Soler <harryalex0281@hotmail.com>
2025-04-07 15:24:03 -0500

    Add wouter frontend package

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,25 @@
+import { Link, Route, Switch } from "wouter";
+
 function App() {
-    return(
-        <></>
-    )
+  return (
+    <>
+      <Link href="/users/1">Profile</Link>
+      <Route path="/users/*">Users</Route>
+
+      <Switch>
+        <Route path="/inbox">
+          Inbox
+        </Route>
+        <Route path="/users/:name">
+          {(params) => <>Hello, {params.name}!</>}
+        </Route>
+
+        <Route>
+          Not found
+        </Route>
+      </Switch>
+    </>
+  );
 }
 
-export default App
+export default App;
