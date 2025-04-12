ca8a2bb9ffa40bf87f86449a3417cfd90919a3ce
Harrizon Soler <harryalex0281@hotmail.com>
2025-04-12 12:35:07 -0500

    Fix helper imports

---
+++ frontend/src/helpers/order.ts
@@ -0,0 +1,27 @@
+import { faker } from "@faker-js/faker";
+import { domain } from "@wailsjs/go/models";
+
+export function getOrderEntry(order: domain.Order): string {
+  return order.OdooReference.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+}
+
+export function getMockOrder(): domain.Order {
+  return new domain.Order({
+    OdooReference: faker.string.nanoid(7),
+    ClientReference: faker.string.uuid(),
+    ClientName: faker.person.fullName(),
+    PlatformName: faker.company.name(),
+    Address: faker.location.streetAddress(),
+    City: faker.location.city(),
+    PhoneNumber: faker.phone.number({ style: "human" }),
+    Products: faker.helpers.multiple(() => ({
+      Name: faker.commerce.productName(),
+      Quantity: faker.number.int({ min: 1, max: 10 }),
+    }), {
+        count: {
+            min: 1,
+            max: 10,
+        }
+    }),
+  });
+}
